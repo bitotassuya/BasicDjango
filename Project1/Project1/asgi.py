ac47dd2483bbf15608f2315c0dc4915a148b1e47
bitotassuya <jareansong@gmail.com>
2022-06-10 13:33:53 +0700

    reset1

--- Project1/Project1/asgi.py
+++ Project1/Project1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for BasicDjangoSMT2 project.
+ASGI config for Project1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BasicDjangoSMT2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project1.settings')
 
 application = get_asgi_application()
--- Project1/Project1/urls.py
+++ Project1/Project1/urls.py
@@ -1,4 +1,4 @@
-"""BasicDjangoSMT2 URL Configuration
+"""Project1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
