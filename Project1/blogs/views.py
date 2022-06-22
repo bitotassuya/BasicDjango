a7164c34be012baaa042cad1443e8f2b2f8643a5
bitotassuya <jareansong@gmail.com>
2022-06-22 16:19:16 +0700

    create Template

--- Project1/blogs/views.py
+++ Project1/blogs/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 
 
 def hello(request):
-    return HttpResponse("<h2>HelloWorld</h2>")
+    return render(request, 'index.html')
